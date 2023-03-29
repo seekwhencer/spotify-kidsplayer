b6cf273d0bdc4bd43e3ee50ce6ae1975200fa351
seekwhencer <matthias.kallenbach@gmail.com>
2023-03-29 20:06:18 +0200

    translations

--- frontend/src/lib/Locale/index.js
+++ frontend/src/lib/Locale/index.js
@@ -1,5 +1,5 @@
 import Translations from './Translations.js';
-import { I18n } from "i18n-js";
+import {I18n} from "i18n-js";
 
 export default class Locale extends MODULECLASS {
     constructor(parent) {
@@ -8,13 +8,14 @@ export default class Locale extends MODULECLASS {
         this.i18n = new I18n();
 
         Object.keys(Translations).forEach(locale => this.i18n.store({
-            [locale] : Translations[locale]
+            [locale]: Translations[locale]
         }));
 
         this.i18n.locale = 'de';
 
         // map it globally
-        window._T = text => this.i18n.t(text);
+        window._T = (text, options) => this.i18n.t(text, options);
+        window._L = (to, value) => this.i18n.l(to, value);
 
     }
 }
